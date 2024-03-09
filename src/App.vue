32b49b7aced6c0d7e08e88c38ab51c6286810ff4
KlaytonJr <klaytonjunior@protonmail.com>
2024-03-09 03:38:37 -0300

    feat: init project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import Navbar from './components/Navbar'
+</script>
+
+<template>
+    <v-app style="background: #F5F5F5">
+        <Navbar />
+              
+        <RouterView id="content"/>
+    </v-app>
+</template>
+
+<style scoped>
+#content {
+  margin-top: 65px;
+}
+</style>
